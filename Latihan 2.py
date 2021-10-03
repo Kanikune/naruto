90a61af18e98ebe717246255e39a95b3db6bf667
Edric <Edricck25@gmail.com>
2021-10-03 12:59:03 +0700

    first commit

---
+++ Latihan 2.py
@@ -0,0 +1,89 @@
+# x = [5,10,15,20,25,30,35,40]           #LIST
+#     #0, 1, 2, 3, 4, 5, 6, 7
+# print(x[5])
+# print(x[-1])
+# print(x[3:5])
+# print(x[3:7])
+# print(x[:5])
+# print(x[-3:])
+# print(x[1:7:3])
+# print(x[7])
+
+
+# x = [1,2,3]         
+# x[0]= 4
+# print (x)
+
+
+# binatang = ['kucing', 'rusa', 'badak', 'gajah']  #DELETRE
+# del binatang[0]
+# del binatang[1]
+# print(binatang)
+
+# s = "Hello World!"
+# print(s[4])           #ambil karakter kelima dari string s
+# print(s[6:11])        #ambil karakter ketujuh hingga kesebelas dari string s
+# #s[0]="d"              #ubah karakter keenam dari string menjai s menjadi "d", seharusnya gagal karna immutable
+
+
+
+# s = "Halo Dunia!"     #ubah isi string s memjadi "Halo Dunia!", seharusnya berhasil karena mutable
+# print (s)
+
+
+# t = (5,'program', 1+3j)        #TUPLE NILAI YG TDK BS DIUBAH
+# print(t[1])
+# print(t[0:3])
+# # print(t[0]=10)
+
+# A = {1,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,5,5,6,7,8,9,0,9,8}                  #SET YG SAMA JADI 1
+# print(A)
+
+# a = {1,2,3}           #SALAH KARENA BERSIFAT UNORDERED, TIDAK BS SLICING 
+# print(a[1])
+
+
+
+# d = {1:'value','key':2}         #DICTIONARY
+# print(type(d))
+
+# d = {1:'value','key':2}          #DICTIONARY
+# print(type(d)) 
+# print("d[1] = ", d[1])
+# print("d['key'] = ", d['key'])
+
+
+# d = {1:'value','key':2}
+# print(type(d))                                        #DICTIONARY BUKAN IMPLEMENTASI URUTAN, TDK BS MENGGUNAKAN URUTUAN INDEKS
+# print("d[1] = ", d[1])
+# print("d['key'] = ", d['key'])
+
+
+# print("d['value'] = ", d['value'])
+
+
+# #generate eror                       karna TDK ADA KUNCI (KEY) 2
+# print("d[2] = ", d[2]);
+
+
+# print(float(5))
+
+# print(int(10.6))
+
+# print(int(-10.6))
+
+# print(float(2.5))
+
+# print(str(25))
+
+# print(int(1p))   
+
+# print(set([1,2,3]))
+
+# print(tuple({5,6,7}))
+
+# print(list('hello'))
+
+# print(dict([[1,2],[3,4]]))
+
+# print(dict([(3,26),(4,44)]))
